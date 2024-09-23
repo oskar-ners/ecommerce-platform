36a99f23836422b69f0914a2c18aeef72eb9bd54
Oskar <ners.kontakt@wp.pl>
2024-09-23 13:55:01 +0200

    footer added

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CategoriesService } from '../../services/categories.service';
 import { Category } from '../../interfaces/category.interface';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
